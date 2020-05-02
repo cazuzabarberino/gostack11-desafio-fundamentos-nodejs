d8401bb7455729d97071d3fb2c811c46518337d8
BroodMaiden <41172576+BroodMaiden@users.noreply.github.com>
2020-05-02 08:22:28 -0300

    desafio completo

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance: Balance = this.transactions.reduce(
+      (prevValue: Balance, transaction: Transaction) => {
+        console.log(transaction);
+        return {
+          ...prevValue,
+          income:
+            prevValue.income +
+            (transaction.type === 'income' ? transaction.value : 0),
+          outcome:
+            prevValue.outcome +
+            (transaction.type === 'outcome' ? transaction.value : 0),
+          total:
+            prevValue.total +
+            (transaction.type === 'income'
+              ? transaction.value
+              : -transaction.value),
+        };
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
