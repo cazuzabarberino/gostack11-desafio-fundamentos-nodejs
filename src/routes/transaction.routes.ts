13a02297dc2471f5c67e6be71f98dba91132bfbc
BroodMaiden <41172576+BroodMaiden@users.noreply.github.com>
2020-05-02 08:30:26 -0300

    desafio completo2

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
+import Transaction from '../models/Transaction';
 
 const transactionRouter = Router();
 
@@ -27,7 +28,7 @@ transactionRouter.post('/', (request, response) => {
 
     const transaction = createTransaction.execute({ title, value, type });
 
-    return response.json({ title, id: transaction.id, value, type });
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
